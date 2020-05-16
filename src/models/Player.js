a83f65e606dad483b752ac2311e71d054e4ad7fc
Doug Tebay <dougtebay@gmail.com>
2020-05-16 10:23:57 -0400

    Add range of motion methods to player

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,4 +1,5 @@
 import Member from './Member';
+import { combinations, range } from '../utilities';
 
 export const PLAYER_NAME = 'player';
 
@@ -8,8 +9,9 @@ class Player extends Member {
 
     this.type = this.actorType;
     this.name = PLAYER_NAME;
+
     this.directions = { left: false, right: false, up: false };
-    this.speed = 0.1;
+    this.movementLength = this.width / 10;
   }
 
   setDirection(direction, isActive) {
@@ -29,15 +31,51 @@ class Player extends Member {
   }
 
   moveLeft() {
-    this.position.subtract({ x: this.speed });
+    this.position = this.position.minus({ x: this.movementLength });
   }
 
   moveRight() {
-    this.position.add({ x: this.speed });
+    this.position = this.position.plus({ x: this.movementLength });
   }
 
   moveUp() {
-    this.position.subtract({ y: this.speed });
+    this.position = this.position.minus({ y: this.movementLength });
+  }
+
+  get rangeOfMotionCoordinates() {
+    const { x, y } = this.rangeOfMotion;
+    const xRange = range(x.start, x.end);
+    const yRange = range(y.start, y.end);
+
+    return combinations(xRange, yRange).map(([x, y]) => ({ x, y }));
+  }
+
+  get rangeOfMotion() {
+    return { ...this.rangeOfHorizontalMotion, ...this.rangeOfVerticalMotion };
+  }
+
+  get rangeOfHorizontalMotion() {
+    return { x: { start: this.rangeOfMotionLeft, end: this.rangeOfMotionRight } };
+  }
+
+  get rangeOfVerticalMotion() {
+    return { y: { start: this.rangeOfMotionUp, end: this.rangeOfMotionDown } };
+  }
+
+  get rangeOfMotionLeft() {
+    return Math.floor(this.leftSide - this.movementLength);
+  }
+
+  get rangeOfMotionRight() {
+    return Math.floor(this.rightSide);
+  }
+
+  get rangeOfMotionUp() {
+    return Math.floor(this.topSide - this.movementLength);
+  }
+
+  get rangeOfMotionDown() {
+    return Math.floor(this.bottomSide);
   }
 }
 
