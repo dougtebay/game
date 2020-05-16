c6d6efe827963ee6e76e03b4b9cabed67612dabe
Doug Tebay <dougtebay@gmail.com>
2020-05-16 09:40:18 -0400

    Use correction factor in floating point math

--- src/models/Position.js
+++ src/models/Position.js
@@ -1,16 +1,28 @@
 class Position {
   constructor({ x, y }) {
     Object.assign(this, { x, y });
+
+    this.correctionFactor = 10;
   }
 
-  add({ x = 0, y = 0 }) {
-    this.x += x;
-    this.y += y;
+  plus({ x = 0, y = 0 }) {
+    return new Position({ x: this.add(this.x, x), y: this.add(this.y, y) });
   }
 
-  subtract({ x = 0, y = 0 }) {
-    this.x -= x;
-    this.y -= y;
+  minus({ x = 0, y = 0 }) {
+    return new Position({ x: this.subtract(this.x, x), y: this.subtract(this.y, y) });
+  }
+
+  add(number1, number2) {
+    return (this.correct(number1) + this.correct(number2)) / this.correctionFactor;
+  }
+
+  subtract(number1, number2) {
+    return (this.correct(number1) - this.correct(number2)) / this.correctionFactor;
+  }
+
+  correct(number) {
+    return number * this.correctionFactor;
   }
 }
 
