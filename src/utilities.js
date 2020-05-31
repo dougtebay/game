bf984e84cbe5c2f7c194d729bcd903fd2584660b
Doug Tebay <dougtebay@gmail.com>
2020-05-31 17:40:44 -0400

    Remove utility functions

--- src/utilities.js
+++
@@ -1,9 +0,0 @@
-export const range = (start, end) => {
-  const items = end - start + 1;
-
-  return Array.from(Array(items), (item, index) => index + start);
-}
-
-export const combinations = (arrayA, arrayB) => {
-  return arrayA.flatMap((arrayAItem) => arrayB.map((arrayBItem) => [arrayAItem, arrayBItem]));
-}
